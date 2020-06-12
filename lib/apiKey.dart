2e86be71874179edddf406bbb2a85061b82c75c0
EDICON <hslee.edicon@gmail.com>
2020-06-12 18:12:18 +0900

    add apiKey

--- lib/apiKey.dart
+++ lib/apiKey.dart
@@ -1,3 +1,4 @@
 import 'dart:io' show Platform;
 
-String apiKey = Platform.environment['api_key'];
+// String apiKey = Platform.environment['api_key'];
+String apiKey = '7075b0fbaedc4e086749c63ece4f660e';
